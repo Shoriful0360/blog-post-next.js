d9d662c25ae5b73408234599b24d0f8b56b34496
Shoriful <shorifulbba0360@gmail.com>
2025-01-24 10:51:12 +0600

    added data fetch

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
+"use client"
 import Image from "next/image";
-
+import blog from './blog.jsx'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link.js";
 export default function Home() {
+const[blogData,setBlogData]=useState()
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>{
+     setBlogData(res.data)
+    })
+  },[])
+  console.log(blogData)
   return (
    <main>
-    <div>
-      Hello world
+    <div className="text-start space-y-2 mt-4  md:w-8/12 mx-auto">
+     {
+      blogData?.map((item,idx)=><li className="list-none " key={item.id}>
+<Link href={''}>{idx + 1}.  <span className="text-red-400">{item.title}</span></Link>
+      </li>)
+     }
     </div>
    </main>
   );
